7a216ba5cd26f8f0ecd4f5a89ccaca2a9099b40f
ilya Pidonenko <ilja.pidonekom@gmail.com>
2023-09-06 08:26:53 +0200

    initial commit :lipstick:

--- src/main/java/ch/csbe/backendlb/Entity/Product/Product.java
+++ src/main/java/ch/csbe/backendlb/Entity/Product/Product.java
@@ -1,6 +1,10 @@
 package ch.csbe.backendlb.Entity.Product;
 
+import io.micrometer.observation.transport.Propagator;
 import jakarta.persistence.*;
+import ch.csbe.backendlb.Entity.Category.Category;
+import org.hibernate.property.access.spi.Setter;
+
 
 @Entity
 public class Product {
@@ -30,5 +34,71 @@ public class Product {
     private Integer stock;
 
     @ManyToOne()
-    private ch.csbe.backendlb.Entity.Category.Category Category;
+    private Category category;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+
+    }
 }
+
--- src/main/java/ch/csbe/backendlb/Entity/Product/ProductRepository.java
+++ src/main/java/ch/csbe/backendlb/Entity/Product/ProductRepository.java
@@ -3,9 +3,23 @@ package ch.csbe.backendlb.Entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-    Product findAllProductsById(Integer id);
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-    @Query("SELECT p FROM Product t WHERE t.title = ?1")
-    Product findProductsById(Integer id);
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findProductsById(Long id);
+    @Query("SELECT p FROM Product  p WHERE p.id= ?1")
+    Product findProductById(Long id);
+    @Query("UPDATE Product p SET p.name = ?2 WHERE p.id = ?1")
+    @Modifying
+    void updateProductNameById(Long id, String newName);
+    @Query("DELETE FROM Product  p WHERE p.id= ?1")
+    @Modifying
+    Product deleteProductById(Long id);
 }
+
